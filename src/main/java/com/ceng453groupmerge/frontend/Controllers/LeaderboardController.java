069b53dc95ca8531937ec40d92e2839e3248a4ee
birkan <birkan.arslan@metu.edu.tr>
2022-06-06 00:16:50 +0300

    list of users and their score as dynamically

--- src/main/java/com/ceng453groupmerge/frontend/Controllers/LeaderboardController.java
+++ src/main/java/com/ceng453groupmerge/frontend/Controllers/LeaderboardController.java
@@ -1,20 +1,25 @@
 package com.ceng453groupmerge.frontend.Controllers;
 
 import com.ceng453groupmerge.frontend.AlertHelper;
-import static com.ceng453groupmerge.frontend.Constants.ErrorConstants.*;
-import static com.ceng453groupmerge.frontend.Constants.fxmlPathConstants.*;
 import com.ceng453groupmerge.frontend.RestClients.LeaderboardRestClient;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Window;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+
+import static com.ceng453groupmerge.frontend.Constants.ErrorConstants.SERVER_NOT_RESPONDING;
+import static com.ceng453groupmerge.frontend.Constants.fxmlPathConstants.MAIN_MENU;
 
 @Component
 public class LeaderboardController {
@@ -26,16 +31,15 @@ public class LeaderboardController {
     public Button weeklyButton;
     @FXML
     public Button mainMenuButton;
+    @FXML
+    public GridPane gridPane;
 
     @FXML
     public void handleAllTimeButtonAction(ActionEvent event) {
         Window owner = allTimeButton.getScene().getWindow();
         try{
             ArrayList<Object> arr = (ArrayList<Object>) LeaderboardRestClient.getAllTimeLeaderboard();
-            for(Object obj:arr) {
-                LinkedHashMap<String,?> hashMap = (LinkedHashMap) obj;
-                System.out.println(hashMap.get("player")+" "+hashMap.get("player_score"));
-            }
+            updateLeaderboard(arr);
         }catch(Exception e){
             AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", SERVER_NOT_RESPONDING);
         }
@@ -46,10 +50,7 @@ public class LeaderboardController {
         Window owner = monthlyButton.getScene().getWindow();
         try{
             ArrayList<Object> arr = (ArrayList<Object>) LeaderboardRestClient.getMonthlyLeaderboard();
-            for(Object obj:arr) {
-                LinkedHashMap<String,?> hashMap = (LinkedHashMap) obj;
-                System.out.println(hashMap.get("player")+" "+hashMap.get("player_score"));
-            }
+            updateLeaderboard(arr);
         }catch(Exception e){
             e.printStackTrace();
             AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", SERVER_NOT_RESPONDING);
@@ -62,10 +63,7 @@ public class LeaderboardController {
         Window owner = weeklyButton.getScene().getWindow();
         try{
             ArrayList<Object> arr = (ArrayList<Object>) LeaderboardRestClient.getWeeklyLeaderboard();
-            for(Object obj:arr) {
-                LinkedHashMap<String,?> hashMap = (LinkedHashMap) obj;
-                System.out.println(hashMap.get("player")+" "+hashMap.get("player_score"));
-            }
+            updateLeaderboard(arr);
         }catch(Exception e){
             AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Error", SERVER_NOT_RESPONDING);
         }
@@ -78,4 +76,36 @@ public class LeaderboardController {
         SceneController.switchToScene(event, MAIN_MENU);
     }
 
+
+    private void updateLeaderboard(ArrayList<Object> arr) {
+        gridPane.getChildren().clear();
+        gridPane.getChildren().removeAll( gridPane.getChildren());
+        int count = 0;
+        for(Object obj:arr) {
+            LinkedHashMap<String,?> hashMap = (LinkedHashMap) obj;
+            VBox vBox1 = new VBox();
+            VBox vBox2 = new VBox();
+            VBox vBox3 = new VBox();
+            if(count%2==0) {
+                vBox1.setStyle("-fx-background-color: #94F799");
+                vBox2.setStyle("-fx-background-color: #94F799");
+                vBox3.setStyle("-fx-background-color: #94F799");
+            } else {
+                vBox1.setStyle("-fx-background-color: #DCF4DD");
+                vBox2.setStyle("-fx-background-color: #DCF4DD");
+                vBox3.setStyle("-fx-background-color: #DCF4DD");
+            }
+            vBox1.alignmentProperty().setValue(Pos.CENTER_LEFT);
+            vBox2.alignmentProperty().setValue(Pos.CENTER_LEFT);
+            vBox3.alignmentProperty().setValue(Pos.CENTER_LEFT);
+            gridPane.addRow(count++,vBox1, vBox2, vBox3);
+            Label label1 = new Label(count+"");
+            label1.paddingProperty().setValue(new Insets(0,0,0,5));
+            vBox1.getChildren().add(label1);
+            vBox2.getChildren().add(new Label(hashMap.get("player").toString()));
+            vBox3.getChildren().add(new Label(hashMap.get("player_score").toString()));
+            System.out.println(hashMap.get("player")+" "+hashMap.get("player_score"));
+//            count++;
+        }
+    }
 }
