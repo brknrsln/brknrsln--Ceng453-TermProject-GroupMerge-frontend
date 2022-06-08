8c0709221d9e1659af7105d561d523fe8821f927
birkan <birkan.arslan@metu.edu.tr>
2022-06-08 23:40:48 +0300

    add new fxml file path

--- src/main/java/com/ceng453groupmerge/frontend/Constants/fxmlPathConstants.java
+++ src/main/java/com/ceng453groupmerge/frontend/Constants/fxmlPathConstants.java
@@ -1,10 +1,12 @@
 package com.ceng453groupmerge.frontend.Constants;
 
 public class fxmlPathConstants {
-    public static final String MAIN_MENU = "/mainMenu.fxml";
-    public static final String GAME = "/game.fxml";
-    public static final String LOGIN_PAGE = "/loginPage.fxml";
-    public static final String FORGOT_PAGE = "/forgotPage.fxml";
-    public static final String REGISTER_PAGE = "/registerPage.fxml";
+    public static final String MAIN_MENU        = "/mainMenu.fxml";
+    public static final String GAME             = "/gamePage.fxml";
+    public static final String LOGIN_PAGE       = "/loginPage.fxml";
+    public static final String FORGOT_PAGE      = "/forgotPage.fxml";
+    public static final String REGISTER_PAGE    = "/registerPage.fxml";
     public static final String LEADERBOARD_PAGE = "/leaderboardsPage.fxml";
+    public static final String DICE             = "/dicePage.fxml";
+    public static final String INFO             = "/infoPage.fxml";
 }
