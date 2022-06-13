8be20456321bfa18dd50e3a6d347741391f61b75
birkan <birkan.arslan@metu.edu.tr>
2022-06-13 10:52:22 +0300

    fixed dice animation and resize dices

--- src/main/java/com/ceng453groupmerge/frontend/Controllers/DiceController.java
+++ src/main/java/com/ceng453groupmerge/frontend/Controllers/DiceController.java
@@ -2,6 +2,7 @@ package com.ceng453groupmerge.frontend.Controllers;
 
 import com.ceng453groupmerge.frontend.GameObjects.Dice;
 import com.ceng453groupmerge.frontend.GameObjects.GameLogic;
+import javafx.animation.AnimationTimer;
 import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
@@ -25,9 +26,12 @@ public class DiceController {
 
     Random random = new Random();
 
+    private Roller clock;
+
     public DiceController() {
         dice = Dice.getInstance();
         diceController = this;
+        clock = new Roller();
     }
 
     public static DiceController getInstance() {
@@ -39,46 +43,46 @@ public class DiceController {
     public void rollDice() {
         System.out.println("Rolled dice");
         GameController.getInstance().setRollButtonDisable(true);
-        int dice1ValueFinal = random.nextInt(6) + 1;
-        int dice2ValueFinal = random.nextInt(6) + 1;
-        dice.setValue1(dice1ValueFinal);
-        dice.setValue2(dice2ValueFinal);
-        double rotate = 24;
+        clock.start();
+    }
 
-        dice1.setImage(dice.getDiceImage(dice1ValueFinal));
-        dice2.setImage(dice.getDiceImage(dice2ValueFinal));
+    private class Roller extends AnimationTimer {
+        private long lastUpdate = 0;
+        private long INTERVAL = 1000000000 / 50;
+        private int MAX_ROLLS = 20;
+        private double angle = 360/MAX_ROLLS;
 
-        RotateTransition rt1 = new RotateTransition(Duration.millis(100), dice1);
-        RotateTransition rt2 = new RotateTransition(Duration.millis(100), dice2);
+        private int counter = 0;
+        private int dice1Value;
+        private int dice2Value;
+        @Override
+        public void handle(long now) {
+            if (now - lastUpdate >= INTERVAL) {
+                dice1Value = random.nextInt(6) + 1;
+                dice2Value = random.nextInt(6) + 1;
 
-        rt1.setByAngle(360);
-        rt2.setByAngle(360);
+                dice1.setImage(dice.getDiceImage(dice1Value));
+                dice2.setImage(dice.getDiceImage(dice2Value));
 
-        rt1.setCycleCount(10);
-        rt2.setCycleCount(10);
+                dice1.rotateProperty().set(dice1.rotateProperty().get() + angle);
+                dice2.rotateProperty().set(dice2.rotateProperty().get() + angle);
 
-        rt2.setOnFinished(e -> GameLogic.getInstance().getPlayers().get(GameLogic.getInstance().getCurrentPlayer()).playTurnAfterDice());
-
-        rt1.play();
-        rt2.play();
-
-        /*Thread thread = new Thread(() -> {
-            try {
-                for (int i = 0; i < 15; i++) {
-                    dice1Value = random.nextInt(6) + 1;
-                    dice2Value = random.nextInt(6) + 1;
-                    dice1.setImage(dice.getDiceImage(dice1Value));
-                    dice2.setImage(dice.getDiceImage(dice2Value));
-                    dice1.rotateProperty().set(dice1.rotateProperty().get() + rotate);
-                    dice2.rotateProperty().set(dice2.rotateProperty().get() + rotate);
-                    Thread.sleep(50);
+                lastUpdate = now;
+                counter++;
+                if (counter >= MAX_ROLLS) {
+                    dice.setValue1(dice1Value);
+                    dice.setValue2(dice2Value);
+                    this.stop();
+                    GameLogic.getInstance().getPlayers().get(GameLogic.getInstance().getCurrentPlayer()).playTurnAfterDice();
+                    counter = 0;
                 }
-                dice1.setImage(dice.getDiceImage(dice1ValueFinal));
-                dice2.setImage(dice.getDiceImage(dice2ValueFinal));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+//                rollDice();
             }
-        });
-        thread.start();*/
+        }
+//            if (dice.isDouble()) {
+//                rollDice();
+//                stop();
+//            }
+//        }
     }
 }
